024986aff57119747bffc321c7cc13a13d1b5d47
Alex V <alexvap23@gmail.com>
2022-01-16 06:48:35 +0200

    init commit, Lesson 104

---
+++ src/actions/index.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+import { FETCH_USER } from './types';
+
+export const fetchUser = () => async dispatch => {
+    const res = await axios.get('/api/current-user');
+
+    dispatch({ type: FETCH_USER, payload: res.data });
+};
+
+export const handleToken = (token) => async dispatch => {
+    console.log('here', token);
+    const res = await axios.post('/api/stripe', token);
+    console.log(res);
+    dispatch({ type: FETCH_USER, payload: res.data });
+};
